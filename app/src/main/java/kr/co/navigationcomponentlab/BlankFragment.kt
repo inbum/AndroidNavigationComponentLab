2562a816b52fedc07c74e6dcbb7360510d133b35
inbum <inbum.o@gmail.com>
2019-08-12 16:17:53 +0900

    first commit

---
+++ app/src/main/java/kr/co/navigationcomponentlab/BlankFragment.kt
@@ -0,0 +1,44 @@
+package kr.co.navigationcomponentlab
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kr.co.navigationcomponentlab.databinding.FragmentBlankBinding
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [BlankFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_blank, container, false)
+        val binding:FragmentBlankBinding = FragmentBlankBinding.inflate(inflater, container, false)
+        binding.clickListener = View.OnClickListener {
+            it.findNavController().navigate(R.id.action_blankFragment_to_blankFragment2)
+        }
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/kr/co/navigationcomponentlab/MainActivity.kt
@@ -0,0 +1,35 @@
+package kr.co.navigationcomponentlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kr.co.navigationcomponentlab.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        // Set up ActionBar
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
